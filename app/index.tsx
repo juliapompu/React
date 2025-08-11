ae09fb6ab5608867637ff2f9d8b0f3d9207702f7
juliapompu <juliapompeusouza200@gmail.com>
2025-08-11 14:16:26 -0300

    aula01,02

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+/*import {View, Text}from 'react-native';
+
+/*export default function index(){
+    return(
+        <View>
+            <Text>Hello World!</Text>  //esse text lança o texto para o app
+        </View>
+    )
+}*/
+
+/* n pode retornar mais de um elemento pai! tipo, n pode ter duas views uma emabixo da outra*/
+
+/*esstilizaçã*/
+
+/*utiliza o css web*/ 
+
+/*<view style = {
+    aligTermente: clearInterval, }>;      /*ele usa os mesmo atributos do css, mas sem o "-" separando as palavras*/ 
+/*</view>
+
+/flex é ativo por padrão*/ 
+
+//title
+//<Button titel="Enviar" onPress={handleMessage}></AButton> //<- assim ele faz um botão onde tada vez que vc clica ele coloca uma mensagem!
+
