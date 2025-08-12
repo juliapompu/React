0428eeecec2a859714875a1bb75fc07a706717ec
juliapompu <juliapompeusouza200@gmail.com>
2025-08-12 16:01:02 -0300

    capitulo 4

--- app/expo.tsx
+++ app/expo.tsx
@@ -326,3 +326,137 @@
 //     alignItems: 'center',
 //   },
 // });
+
+
+//CAPITULO 4
+
+// // ...rest of the import statements remain unchanged
+//  import * as ImagePicker from 'expo-image-picker'; <- IMPORTANDO DAQUI: npx expo install expo-image-picker
+
+// export default function Index() { <-passa a imagem para o para o Launch
+//   const pickImageAsync = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ['images'],
+//       allowsEditing: true,
+//       quality: 1,
+//     });
+
+//     if (!result.canceled) {<- se a imagem for escolhida ele printa a mensagem
+//       console.log(result);
+//     } else {
+//       alert('You did not select any image.');
+//     }
+//   };
+
+//   // ...rest of the code remains same
+// }
+
+
+// import { View, StyleSheet } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+
+// import Button from '@/components/Button';
+// import ImageViewer from '@/components/ImageViewer';
+
+// const PlaceholderImage = require('@/assets/images/background-image.png');
+
+// export default function Index() {
+//   const pickImageAsync = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ['images'],
+//       allowsEditing: true,
+//       quality: 1,
+//     });
+
+//     if (!result.canceled) {
+//       console.log(result);
+//     } else {
+//       alert('You did not select any image.');
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <ImageViewer imgSource={PlaceholderImage} />
+//       </View>
+//       <View style={styles.footerContainer}>
+//         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+//         <Button label="Use this photo" />
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//   },
+//   footerContainer: {
+//     flex: 1 / 3,
+//     alignItems: 'center',
+//   },
+// });
+
+
+// import { View, StyleSheet } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+
+// import { useState } from 'react';
+
+
+// import Button from '@/components/Button';
+// import ImageViewer from '@/components/ImageViewer';
+
+// const PlaceholderImage = require('@/assets/images/background-image.png');
+
+// export default function Index() {
+//   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);<- Cria uma variavelç que vai "hold" a imagem 
+
+
+//   const pickImageAsync = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ['images'],
+//       allowsEditing: true,
+//       quality: 1,
+//     });
+
+//     if (!result.canceled) {<- 
+//       setSelectedImage(result.assets[0].uri);<-Pick the first uri from the assets array. Also, there is only one image selected at a time so you don't have to change this
+//     } else {
+//       alert('You did not select any image.');
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} /><-Pass the selected image URI to the ImageViewer component.
+//       </View>
+//       <View style={styles.footerContainer}>
+//         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+//         <Button label="Use this photo" />
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//   },
+//   footerContainer: {
+//     flex: 1 / 3,
+//     alignItems: 'center',
+//   },
+// });
--- components/expo.tsx
+++ components/expo.tsx
@@ -18,7 +18,7 @@
 // });
 
 
-//BOTÃO
+                //BOTÃO
 //componente para o botão onde a gente depos chama la no index.tsx
 // import { StyleSheet, View, Pressable, Text } from 'react-native';
 
@@ -60,7 +60,7 @@
 // });
 
 
-//BOTAO
+            //BOTAO
 // import { StyleSheet, View, Pressable, Text } from 'react-native';
 // import FontAwesome from '@expo/vector-icons/FontAwesome'; <- ELE EXPORTA DA "FontAwesome" PRA GENTE
 
@@ -119,6 +119,90 @@
 //   buttonLabel: {
 //     color: '#fff',
 //     fontSize: 16,
+//   },
+// });
+
+                //IMAGEM
+
+// import { StyleSheet, View, Pressable, Text } from 'react-native';
+// import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+// type Props = {
+//   label: string;
+//   theme?: 'primary';
+//   onPress?: () => void;<- adiciona a prop onPress
+// };
+
+// export default function Button({ label, theme, onPress }: Props) {
+//   if (theme === 'primary') {
+//     return (
+//       <View
+//         style={[
+//           styles.buttonContainer,
+//           { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+//         ]}>
+//         <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
+//           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+//           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+//         </Pressable>
+//       </View>
+//     );
+//   }
+
+//   return (
+//     <View style={styles.buttonContainer}>
+//       <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+//         <Text style={styles.buttonLabel}>{label}</Text>
+//       </Pressable>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   buttonContainer: {
+//     width: 320,
+//     height: 68,
+//     marginHorizontal: 20,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 3,
+//   },
+//   button: {
+//     borderRadius: 10,
+//     width: '100%',
+//     height: '100%',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     flexDirection: 'row',
+//   },
+//   buttonIcon: {
+//     paddingRight: 8,
+//   },
+//   buttonLabel: {
+//     color: '#fff',
+//     fontSize: 16,
+//   },
+// });
+
+// import { ImageSourcePropType, StyleSheet } from 'react-native';
+// import { Image } from 'expo-image';
+
+// type Props = {
+//   imgSource: ImageSourcePropType;
+//   selectedImage?: string;
+// };
+
+// export default function ImageViewer({ imgSource, selectedImage }: Props) {
+//   const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+//   return <Image source={imageSource} style={styles.image} />;
+// }
+
+// const styles = StyleSheet.create({
+//   image: {
+//     width: 320,
+//     height: 440,
+//     borderRadius: 18,
 //   },
 // });
 
