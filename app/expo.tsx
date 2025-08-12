e9538397bc0d51815a7d1d3bdb6dd39360b7d57a
juliapompu <juliapompeusouza200@gmail.com>
2025-08-12 15:40:31 -0300

    capitulo 3

--- app/expo.tsx
+++ app/expo.tsx
@@ -235,7 +235,7 @@
 //   return (
 //     <View style={styles.container}>
 //       <View style={styles.imageContainer}>
-//         <ImageViewer imgSource={PlaceholderImage} />
+//         <ImageViewer imgSource={PlaceholderImage} /> <- coloca a imagem no lugar
 //       </View>
 //     </View>
 //   );
@@ -249,5 +249,80 @@
 //   },
 //   imageContainer: {
 //     flex: 1,
+//   },
+// });
+
+
+// import { View, StyleSheet } from 'react-native';
+
+// import Button from '@/components/Button'; 
+// import ImageViewer from '@/components/ImageViewer';
+
+// const PlaceholderImage = require("@/assets/images/background-image.png");
+
+// export default function Index() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <ImageViewer imgSource={PlaceholderImage} />
+//       </View>
+//       <View style={styles.footerContainer}>
+//         <Button label="Choose a photo" /> <- aqui é o componente que a gente criou la no outro arquivo, fazendo assim ele criar dois botões com o mesmo componente;
+//         <Button label="Use this photo" />
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//     paddingTop: 28,
+//   },
+//   footerContainer: {
+//     flex: 1 / 3,
+//     alignItems: 'center', <-adiciona unm estilo diferente!
+//   },
+// });
+
+
+// import { View, StyleSheet } from 'react-native';
+
+// import Button from '@/components/Button';
+// import ImageViewer from '@/components/ImageViewer';
+
+// const PlaceholderImage = require('@/assets/images/background-image.png');
+
+// export default function Index() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <ImageViewer imgSource={PlaceholderImage} />
+//       </View>
+//       <View style={styles.footerContainer}>
+//         <Button theme="primary" label="Choose a photo" />
+//         <Button label="Use this photo" />
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//   },
+//   footerContainer: {
+//     flex: 1 / 3,
+//     alignItems: 'center',
 //   },
 // });
